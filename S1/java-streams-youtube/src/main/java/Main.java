a6de952c7e8ecce79a4252e4438332f33500e876
IGDERZEN-Zakaria <zakaria.igderzen@gmail.com>
2022-05-26 09:30:40 +0100

    start

---
+++ S1/java-streams-youtube/src/main/java/Main.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Main {
+
+  public static void main(String[] args) {
+    List<Person> people = getPeople();
+
+//////////////////////////////////////////////////////////////////////
+    // Imperative approach ❌
+//////////////////////////////////////////////////////////////////////
+
+/*    List<Person> females = new ArrayList<>();
+
+    for (Person person : people) {
+
+      if (person.getGender().equals(Gender.FEMALE)) {
+        females.add(person);
+      }
+    }
+    females.forEach(System.out::println);*/
+
+
+//////////////////////////////////////////////////////////////////////
+    // Declarative approach ✅
+//////////////////////////////////////////////////////////////////////
+
+    // Filter
+    List<Person> females = people.stream()
+        .filter(person -> person.getGender().equals(Gender.FEMALE))
+        .collect(Collectors.toList());
+
+    //females.forEach(System.out::println);
+
+    // Sort
+    List<Person> sorted = people.stream()
+        .sorted(Comparator.comparing(Person::getAge).thenComparing(Comparator.comparing(Person::getGender).reversed()))
+        .collect(Collectors.toList());
+
+    //sorted.forEach(System.out::println);
+
+
+    // Sort
+    List<Person> sortedReverseSecondCondition = people.stream()
+            .sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getGender))
+            .collect(Collectors.toList());
+
+    System.out.println("");
+    //sortedReverseSecondCondition.forEach(System.out::println);
+
+    // All match
+    boolean allMatch = people.stream()
+        .allMatch(person -> person.getAge() > 8);
+
+    //System.out.println(allMatch);
+    // Any match
+    boolean anyMatch = people.stream()
+        .anyMatch(person -> person.getAge() > 121);
+
+    //System.out.println(anyMatch);
+    // None match
+    boolean noneMatch = people.stream()
+        .noneMatch(person -> person.getName().equals("Zack"));
+
+    //System.out.println(noneMatch);
+
+    // Max
+    people.stream()
+        .max(Comparator.comparing(Person::getAge));
+    //.ifPresent(System.out::println);
+
+    // Min
+    people.stream()
+        .min(Comparator.comparing(Person::getAge));
+//        .ifPresent(System.out::println);
+
+    // Group
+    Map<Gender, List<Person>> groupByGender = people.stream()
+        .collect(Collectors.groupingBy(Person::getGender));
+
+      groupByGender.forEach((gender, people1) -> {
+      System.out.println(gender);
+      people1.forEach(System.out::println);
+      System.out.println();
+    });
+
+    Optional<String> OldestFemale = people.stream().
+            filter(person -> person.getGender().equals(Gender.FEMALE)).
+            max(Comparator.comparing(Person::getAge)).
+            map(Person::getName);
+
+    OldestFemale.ifPresent(System.out::println);
+
+
+
+
+  }
+
+  private static List<Person> getPeople() {
+    return List.of(
+        new Person("Antonio", 20, Gender.MALE),
+        new Person("Alina Smith", 33, Gender.FEMALE),
+        new Person("Helen White", 57, Gender.FEMALE),
+        new Person("Alex Boz", 14, Gender.MALE),
+        new Person("Anna Cook", 99, Gender.FEMALE),
+        new Person("Jamie Goa", 99, Gender.MALE),
+        new Person("Zelda Brown", 99, Gender.FEMALE)
+    );
+  }
+
+}
